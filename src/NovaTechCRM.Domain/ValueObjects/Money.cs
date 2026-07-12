617c1f7a24f31b2cc5931fa7ebb3751a185ad03a
Arslan Hanif <arslanhanif567@gmail.com>
2026-07-12 16:35:23 +0500

    correction in return

--- src/NovaTechCRM.Domain/ValueObjects/Money.cs
+++ src/NovaTechCRM.Domain/ValueObjects/Money.cs
@@ -37,6 +37,7 @@ public readonly struct Money : IEquatable<Money>, IComparable<Money>
     {
         if (discountPercent < 0 || discountPercent > 100)
             throw new ArgumentOutOfRangeException(nameof(discountPercent));
+        //return new Money(Amount * (1 - discountPercent / 100), Currency);
         return new Money(Amount * (1 - discountPercent / 100), Currency);
     }
 
