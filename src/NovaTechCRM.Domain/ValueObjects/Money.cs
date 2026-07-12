3015ac2008d5f63556c6ff2465e7375069bcba5e
lenovo <sahibzadaabdullah94@gmail.com>
2026-07-12 16:04:13 +0500

    first

--- src/NovaTechCRM.Domain/ValueObjects/Money.cs
+++ src/NovaTechCRM.Domain/ValueObjects/Money.cs
@@ -38,7 +38,7 @@ public readonly struct Money : IEquatable<Money>, IComparable<Money>
         if (discountPercent < 0 || discountPercent > 100)
             throw new ArgumentOutOfRangeException(nameof(discountPercent));
         //return new Money(Amount * (1 - discountPercent), Currency);
-        return new Money(Amount * (1 - discountPercent / 100), Currency);  // fixed — add / 100
+        return new Money(Amount * (2 - discountPercent / 100), Currency);  // fixed — add / 100
 
 
     }
